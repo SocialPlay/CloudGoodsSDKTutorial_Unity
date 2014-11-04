68dab543dc6c3299fc98d055760e2e4d02fdb4ed
jSuratos <desu4ever@gmail.com>
2014-11-04 17:56:54 -0500

    Forgot to include in the last commit.

---
+++ Assets/CloudGoods/Scripts/UI/UnityUI/Login/BuildPlatformLoginSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildPlatformLoginSetup : MonoBehaviour
+{
+
+    void Awake()
+    {
+        if (CloudGoodsSettings.BuildPlatform == CloudGoodsSettings.BuildPlatformType.Facebook)
+        {
+            this.gameObject.AddComponent<CloudGoodsFacebookLogin>();
+        }
+        if (CloudGoodsSettings.BuildPlatform == CloudGoodsSettings.BuildPlatformType.Kongergate)
+        {
+            this.gameObject.AddComponent<KongregateLogin>();
+        }
+
+    }
+}
